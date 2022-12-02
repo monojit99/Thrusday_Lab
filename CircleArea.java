2a099023b0a0ba94966ad9323c3fb1649051cecb
MONOJIT CHOWDHURY <55353767+monojitchowdhury@users.noreply.github.com>
2022-12-02 11:02:53 +0530

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,10 @@
+public class CircleArea
+{
+public static void main(String args[])
+{
+double pi=1.732;
+float r=4;
+double area=pi*r*r;
+System.out.println("The Area of circle is:"+area);
+}
+}
---
+++ RectPeri.java
@@ -0,0 +1,9 @@
+public class RectPeri
+{
+public static void main(String args[])
+{
+int l=6,b=5;
+int p=2*(l+b);
+System.out.println("The perimeter of Rectangle is:"+p);
+}
+}
