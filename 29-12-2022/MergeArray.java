996e5af5b1eae7bfba514ddc6f2d28bec8ad4985
MONOJIT CHOWDHURY <55353767+monojit99@users.noreply.github.com>
2022-12-29 13:30:12 +0530

    Update MergeArray.java

--- 29-12-2022/MergeArray.java
+++ 29-12-2022/MergeArray.java
@@ -4,9 +4,9 @@ public class MergeArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		  int[]a = {5,6,4,8,5,2,9};
-	      int[]b = {7,3,6,8,5,3,2};
-	      int[]c = new int[a.length+b.length];
+	      int[]a = {5,6,4,8,5,2,9}; //take first array
+	      int[]b = {7,3,6,8,5,3,2};  //take second array
+	      int[]c = new int[a.length+b.length]; //merge the array
 	      int count = 0;
 	      
 	      for(int i = 0; i < a.length; i++) { 
@@ -14,7 +14,7 @@ public class MergeArray {
 	         count++;
 	      } 
 	      for(int j = 0; j < b.length;j++) { 
-	         c[count++] = b[j];
+	         c[count++] = b[j]; //merge the second array from the last index of the first array
 	      } 
 	      for(int i = 0;i < c.length;i++) 
 	    	 System.out.print(c[i]+" ");
