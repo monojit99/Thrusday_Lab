7f3c29eb609f14f7c131091e1dc405ea5e80742a
MONOJIT CHOWDHURY <55353767+monojit99@users.noreply.github.com>
2022-12-29 13:26:57 +0530

    Update ArrayElementInsertion.java

--- 29-12-2022/ArrayElementInsertion.java
+++ 29-12-2022/ArrayElementInsertion.java
@@ -6,18 +6,18 @@ public class ArrayElementInsertion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n, pos, x;
+	int n, pos, x;
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter no. of elements you want in array:");
+        System.out.print("Enter no. of elements you want in array:"); //take input
         n = s.nextInt();
         int a[] = new int[n+1];
         System.out.println("Enter all the elements:");
         for(int i = 0; i < n; i++)
         {
-            a[i] = s.nextInt();
+            a[i] = s.nextInt(); //store the element in perfect index position
         }
         System.out.print("Enter the position where you want to insert element:");
-        pos = s.nextInt();
+        pos = s.nextInt(); //add the element in given index position
         System.out.print("Enter the element you want to insert:");
         x = s.nextInt();
         for(int i = (n-1); i >= (pos-1); i--)
@@ -28,7 +28,7 @@ public class ArrayElementInsertion {
         System.out.print("After inserting:");
         for(int i = 0; i < n; i++)
         {
-            System.out.print(a[i]+",");
+            System.out.print(a[i]+","); //print all the elements after inserting
         }
         System.out.print(a[n]);
     }
