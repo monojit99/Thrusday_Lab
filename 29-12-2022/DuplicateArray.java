90fc2546267106e66279752960bb436609d17a6a
MONOJIT CHOWDHURY <55353767+monojit99@users.noreply.github.com>
2022-12-29 13:28:10 +0530

    Update DuplicateArray.java

--- 29-12-2022/DuplicateArray.java
+++ 29-12-2022/DuplicateArray.java
@@ -11,7 +11,7 @@ public class DuplicateArray {
         for(int i = 0; i < arr.length; i++) {  
             for(int j = i + 1; j < arr.length; j++) {  
                 if(arr[i] == arr[j])  
-                    System.out.println(arr[j]);  
+                    System.out.println(arr[j]);  //print no. of duplicate element
             }  
         }  
 	}
